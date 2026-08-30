dd6ca5b020d8d013b2ed82e789a7ef90301ebb09
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2026-08-30 15:46:51 -0300

    Fix output type in font conversion script

--- Utilities/bdf2hex.py
+++ Utilities/bdf2hex.py
@@ -49,7 +49,7 @@ bytes_per_row = (width + 7) // 8
 with open(output_file, "w") as out:
     out.write(f"#define WFONT {width}\n")
     out.write(f"#define HFONT {height}\n\n")
-    out.write("uint8_t font[] = {\n")
+    out.write("unsigned char font[] = {\n")
 
     for ch in range(256):
 
